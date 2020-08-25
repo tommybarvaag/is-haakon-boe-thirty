227925c53097b9cd2f70b5281116414eebf01365
Tommy Barvåg <tommy@barvaag.com>
2020-08-25 18:30:27 +0200

    Trying with polyfill

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,27 @@
+import * as React from "react";
 import "../styles/globals.css";
 import CountdownProvider from "../utils/countdownProvider";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const cssScrollSnapPolyfillAdded = React.createRef(false);
+
+  React.useEffect(() => {
+    if (!cssScrollSnapPolyfillAdded.current) {
+      console.log("Add poly");
+      import("css-scroll-snap-polyfill").then(mod => {
+        mod.default();
+        cssScrollSnapPolyfillAdded.current = true;
+      });
+    }
+
+    return () => {
+      cssScrollSnapPolyfillAdded.current = false;
+    };
+  }, [cssScrollSnapPolyfillAdded.current]);
+
   return (
     <CountdownProvider deadlineDate={new Date(2020, 7, 26, 0, 0, 0)}>
       <Component {...pageProps} />
     </CountdownProvider>
   );
 }
-
-export default MyApp;
