61eb6fe64d89bc26d7201cf8b3fa7eecc807c33f
Tommy Barvåg <tommy@barvaag.com>
2020-08-25 16:42:15 +0200

    Add better endings to countdown text

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -14,19 +14,19 @@ export default function Countdown({ hideCountdownOnDeadlineReached }) {
       <div className={styles.container}>
         <div className={styles.item}>
           <div className={styles.value}>{timeToDeadline?.days}</div>
-          <div className={styles.text}>dager</div>
+          <div className={styles.text}>{timeToDeadline?.days === 1 ? "dag" : "dager"}</div>
         </div>
         <div className={styles.item}>
           <div className={styles.value}>{timeToDeadline?.hours}</div>
-          <div className={styles.text}>timer</div>
+          <div className={styles.text}>{timeToDeadline?.hours === 1 ? "time" : "timer"}</div>
         </div>
         <div className={styles.item}>
           <div className={styles.value}>{timeToDeadline?.minutes}</div>
-          <div className={styles.text}>minutter</div>
+          <div className={styles.text}>{timeToDeadline?.minutes === 1 ? "minutt" : "minutter"}</div>
         </div>
         <div className={styles.item}>
           <div className={styles.value}>{timeToDeadline?.seconds}</div>
-          <div className={styles.text}>sekunder</div>
+          <div className={styles.text}>{timeToDeadline?.seconds === 1 ? "sekund" : "sekunder"}</div>
         </div>
       </div>
     );
